fed014e40eabe64ca4b75260b2528b0ebba5ad7a
nithish125 <nithishs3620@gmail.com>
2022-06-06 12:16:22 +0530

    update

---
+++ reversr.js
@@ -0,0 +1,50 @@
+let a;
+let i;
+a = prompt("Enter the Value of n", "");
+console.log(+a);
+while (a != 0) {
+  reminder = a % 10; 
+  a = a - reminder; 
+  a = a / 10; 
+  switch (reminder) {
+    case 0:
+      console.log("zero");
+      break;
+
+    case 1:
+      console.log("one");
+      break;
+
+    case 2:
+      console.log("two");
+      break;
+
+    case 3:
+      console.log("three");
+      break;
+
+    case 4:
+      console.log("four");
+      break;
+
+    case 5:
+      console.log("five");
+      break;
+
+    case 6:
+      console.log("six");
+      break;
+
+    case 7:
+      console.log("seven");
+      break;
+
+    case 8:
+      console.log("eight");
+      break;
+
+    case 9:
+      console.log("nine");
+      break;
+  }
+}
